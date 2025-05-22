f0bf7970de333fbfd7985ea009c8c03697057862
Abhir Iyer <98158477+abhir-iyer@users.noreply.github.com>
2025-05-22 10:12:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, origins=["https://geobias-frontend.vercel.app"])  # Allow frontend requests from any origin
+CORS(app, origins=["https://geobias-frontend.vercel.app"])
 
 # Load data once at startup
 df = pd.read_csv('data/geobias_matrix.csv')
